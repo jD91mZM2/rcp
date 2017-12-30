c53396acada7bc8d6a716b37ac4b279a5d742ff6
inferiormartin <maarten@mrtn.run>
2017-12-30 15:36:36 +0100

    temporary trash

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate clap;
 
 use std::fs;
+use std::path::Path;
 use clap::{App, Arg};
 
 fn main() {
@@ -11,15 +12,46 @@ fn main() {
         .author(crate_authors!())
         .arg(Arg::with_name("from")
             .help("The file you want to copy")
-            .index(1)
-            .required(true))
+            .index(1))
         .arg(Arg::with_name("to")
             .help("The destination")
-            .index(2)
-            .required(true))
+            .index(2))
+        .arg(Arg::with_name("paste")
+            .short("p")
+            .long("paste"))
         .get_matches();
 
-    copy_to(matches.value_of("from").unwrap(), matches.value_of("to").unwrap());
+    if matches.is_present("from") && matches.is_present("to") {
+        copy_to(matches.value_of("from").unwrap(), matches.value_of("to").unwrap());
+    } else if matches.is_present("from") {
+        let from = matches.value_of("from").unwrap();
+        let items: Vec<_> = from.split("/").collect();
+        let name = items.last().unwrap();
+        copy_to(from, &format!("/tmp/rcp/{}", name));
+    } else if matches.is_present("paste") {
+        let dir = Path::new("/tmp/rcp/");
+        if !dir.exists() {
+            fs::create_dir(dir).unwrap();
+        }
+        let paths = fs::read_dir("/tmp/rcp/").unwrap();
+        let cur_dir = Path::new(".");
+        let mut fail = false;
+        for path in paths {
+            let path = path.unwrap();
+            let file_name = path.file_name();
+            let path = path.path();
+            let new_file = cur_dir.join(file_name);
+            if !fail {
+                if let Err(_) = fs::rename(&path, &new_file) {
+                    fail = true;
+                }
+            }
+            if fail {
+                fs::copy(&path, &new_file).unwrap();
+                fs::remove_file(path).unwrap();
+            }
+        }
+    }
 }
 
 fn copy_to(from: &str, to: &str) {
